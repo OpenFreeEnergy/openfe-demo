b172c10fc7d15fa89703f97ff39766f4cf5d874f
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-06-10 15:59:37 -0700

    add some text

--- src/openfe-demo.py
+++ src/openfe-demo.py
@@ -30,11 +30,31 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    First, we'll demonstrate runing a basic RBFE campaign from the command line (with no Python at all!).
+    The OpenFE ecosystem is an open-source framework for calculating alchemical free energies.
 
-    The 3 stages described above each correspond to a CLI command:
 
-    **1. setup**: ``openfe plan-rbfe-network``
+    Binding free energies are one metric used to predict which ligand might bind best to a target protein and are often used in **computational drug discovery campaigns**.
+
+    This notebook demonstrates how you can use either the **CLI or Python API** to execute physics-based simulations and calculate relative binding free energies (RBFE) for a series of ligands and a given protein.
+
+
+    - ``.sdf`` format: [chemical data file format](https://en.wikipedia.org/wiki/Chemical_table_file#SDF) used to describe small molecules.
+    - ``.pdb`` format: [protein data bank](https://www.wwpdb.org/) standardized file format for describing proteins:
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    OpenFE free energy calculations can be thought of as 3 distinct steps, each of which corresponds to a CLI command.
+
+
+    1. **Plan**: `openfe plan-rbfe-networl` construct a graph relating the ligands
+    2. **Run**: execute a physics-based simulation
+    3. **Gather**: compute meaningful metrics from the simulation data
+
+    **1. **: ``openfe plan-rbfe-network``
 
     **2. run**: ``openfe quickrun``
 
@@ -45,19 +65,19 @@ def _(mo):
     return
 
 
-@app.cell
-def _():
-    import subprocess
-
-    subprocess.run("openfe")
-    # subprocess.run(["openfe plan-rbfe-network -M inputs/tyk2_ligands_charged.sdf -p inputs/tyk2_protein.pdb -o tyk2_campaign"])
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## 1. Running a relative binding free energy campaign from a candidate ligand
+    """)
     return
 
 
-@app.cell
-def _():
-
-
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+ 
+    """)
     return
 
 
